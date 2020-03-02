d80252890757c2007367ca0e968ebf34658c7d2c
Isaiah Meek <50851883+Isaiah-DSW@users.noreply.github.com>
2020-03-02 23:12:31 +1300

    Add files via upload

--- script.js
+++ script.js
@@ -22,18 +22,27 @@ var message;
 var currentURL;
 var timestamp;
 var cache;
+var loaded = 0;
+var openSrc = '';
+var cacheImg = '';
+
 function loadImages() {
 	closeNav();
+	document.getElementById('loader').style.opacity = 0;
+	document.getElementById('loader').style.pointerEvents = 'none';
 	document.getElementById('navcircle').style.display = 'none';
 	setTimeout(function() {
 		timestamp = new Date().getTime();
 		while (i < count) {
 			i++;
 			filename = "https://www.trafficnz.info/camera/" + i + ".jpg";
-			$('.container').append('<img class=img src="' + filename + '?t=' + timestamp + '" onError="removeElement(this)" onLoad="increaseCount(this)" />');
+			$('.container').append('<img class=img src="' + filename + '?t=' + timestamp + '" onError="removeElement(this)" onLoad="increaseCount(this)" onClick="openImg(this)" />');
 		}
+		document.getElementById('reloadbutton').style.display = 'block';
+		document.getElementById('loadbutton').style.display = 'none';
 		setTimeout(function() {
 			document.getElementById('navcircle').style.display = 'block';
+			loaded = 1;
 		}, 600);
 	}, 600);
 }
@@ -51,8 +60,32 @@ function increaseCount(question) {
 
 setInterval(function() {
 	images = document.querySelectorAll(".container img");
-	document.getElementById('count').innerHTML = images.length + ' Images Loaded';
+	document.getElementById('count').innerHTML = 'Checking ' + images.length + ' Cameras';
 }, 500);
+
+setInterval(function() {
+	if (loaded == 1) {
+		a = 0;
+		document.getElementById('menuimg').style.display = 'none';
+		document.getElementById('loadimg').style.display = 'block';
+		timestamp = new Date().getTime();
+		while (a < images.length) {
+			currentUrl = document.getElementsByClassName("img")[a].src;
+			cache = currentUrl.indexOf('?');
+			currentUrl = currentUrl.substring(0, cache != -1 ? cache : currentUrl.length);
+			currentUrl = currentUrl + '?t=' + timestamp;
+			document.getElementsByClassName("img")[a].src = currentUrl;
+			a++;
+		}
+		opensrc = opensrc + '?t=' + timestamp;
+		opensrc = 'url("' + opensrc + '")';
+		document.getElementById('popup').style.backgroundImage = opensrc;
+		setTimeout(function() {
+			document.getElementById('menuimg').style.display = 'block';
+			document.getElementById('loadimg').style.display = 'none';
+		}, 600);
+	}
+}, 45000);
 
 function reloadImages() {
 	closeNav();
@@ -75,11 +108,26 @@ function reloadImages() {
 	}, 600);
 }
 
+function openImg(question) {
+	document.getElementById('popup').style.backgroundImage = 'url("' + question.src + '")';
+	document.getElementById('popup').style.opacity = 1;
+	document.getElementById('popupbacking').style.opacity = 0.8;
+	document.getElementById('popupbacking').style.pointerEvents = 'auto';
+	opensrc = question.src;
+	cacheImg = opensrc.indexOf('?');
+	opensrc = opensrc.substring(0, cacheImg != -1 ? cacheImg : opensrc.length);
+}
+
+function closeImg() {
+	document.getElementById('popup').style.opacity = 0;
+	document.getElementById('popupbacking').style.opacity = 0;
+	document.getElementById('popupbacking').style.pointerEvents = 'none';
+}
+
 window.onload = function() {
-  document.getElementsByClassName('container')[0].style.columnCount = 5;
-  //document.documentElement.style.overflowY = 'scroll';
   setTimeout(function() {
-    document.getElementById('load').style.opacity = 0;
-    document.getElementById('load').style.pointerEvents = 'none';
+	setInterval(function() {
+		document.getElementsByClassName('container')[0].style.columnCount = document.getElementById('columnslider').value;
+	}, 500);
   }, 10);
 }
